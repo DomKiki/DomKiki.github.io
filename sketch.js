9a482bfeb14f81da810b83bcccaaae693e5addf6
Dom <DomKiki42@gmail.com>
2019-05-15 19:38:03 +0200

    Resize banner
    Well that actually worked, but the sea is too big so here's a cropped version

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 /****************************** Banner *******************************/
 
-const IMG_SKY = DIR_IMG + "sky.png"; // "https://image.noelshack.com/fichiers/2019/20/3/1557939925-sky.png",
-	  IMG_SEA = DIR_IMG + "sea.png"; // "https://image.noelshack.com/fichiers/2019/20/3/1557939925-sea.png",
-	  IMG_TXT = DIR_IMG + "txt.png"; // "https://image.noelshack.com/fichiers/2019/20/3/1557939924-text.png";
+const IMG_SKY = DIR_IMG + "sky.png";
+	  IMG_SEA = DIR_IMG + "sea.png";
+	  IMG_TXT = DIR_IMG + "txt.png";
 	  
 var   canvas,
 	  imgSky, imgSea, imgTxt;
@@ -10,12 +10,12 @@ var   canvas,
 function preload() {
 	imgSky = loadImage(IMG_SKY);
 	imgSea = loadImage(IMG_SEA);
-	//imgTxt = loadImage(IMG_TXT);
+	imgTxt = loadImage(IMG_TXT);
 }
 	  
 function setup() {
 
-	canvas = createCanvas(800, 400);
+	canvas = createCanvas(800, 325);
 	canvas.parent("#banner");
 	
 	for (var entry of entries)
@@ -26,8 +26,8 @@ function setup() {
 function draw() {
 
 	image(imgSky,0,0);
+	image(imgTxt,0,0);
 	image(imgSea,0,225);
-	//image(imgTxt,0,225);
 	
 	noLoop();
 
